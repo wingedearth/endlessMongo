63c03a54968de494988504d4dfec8eb791ec3cb7
Andrew Anissi <andrew@wingedearth.com>
2015-11-12 23:35:52 -0800

    first commit.

---
+++ seeds.js
@@ -0,0 +1,33 @@
+
+
+
+var users = [
+  { // 0
+    handle: "Destiny",
+    email: "destiny@endless.com"
+  },
+  {
+    handle: "Death",
+    email: "death@endless.com"
+  },
+  {
+    handle: "Dream",
+    email: "dream@endless.com"
+  },
+  {
+    handle: "Destruction",
+    email: "destruction@endless.com"
+  },
+  {
+    handle: "Desire",
+    email: "desire@endless.com"
+  },
+  {
+    handle: "Despair",
+    email: "despair@endless.com"
+  },
+  {
+    handle: "Delirium",
+    email: "delirium@endless.com"
+  }
+]
